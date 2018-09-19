c9d80af9486ff3f9d01adde00678fd33a2b071e1
redrei <spillesen@gmail.com>
2018-09-19 10:40:33 +0200

    j

--- Hitratunnel/HitratunnelNy.java
+++ Hitratunnel/HitratunnelNy.java
@@ -54,8 +54,8 @@ public class HitratunnelNy{
 				Thread.sleep(500);
 
 
-				Motor.A.rotate(240);
-				Motor.C.rotate(-240);
+				Motor.A.rotate(235);
+				Motor.C.rotate(-235);
 				while(Motor.A.isMoving())Thread.yield();
 			}
 			trykksensor.fetchSample(trykkSample, 0);
