211ffbbab2b8bf71833fc8f059ec8aca2107709d
redrei <spillesen@gmail.com>
2018-09-25 14:12:52 +0200

    au.wav

--- rally/relly.java
+++ rally/relly.java
@@ -9,6 +9,7 @@ import lejos.hardware.Button;
 import java.io.*;
 import lejos.hardware.Sound.*;
 import lejos.hardware.sensor.NXTLightSensor;
+import lejos.hardware.sensor.NXTSoundSensor;
 
 public class relly {
 	public static void main (String[] args)
@@ -19,7 +20,7 @@ public class relly {
 //setter motorfart A til 200 og B til 300.
 
 	 	Brick brick = BrickFinder.getDefault();
-	 	Port s1 = brick.getPort("S1");
+	 	Port s1 = brick.getPort("S3");
 	 	Port s2 = brick.getPort("S2");
 //finner hvilke sensorer som er koblet til
 
@@ -42,7 +43,7 @@ public class relly {
 	   		fargeLeser.fetchSample(fargeSample, 0);
 			lysLeser.fetchSample(lysSample, 0);
        		if (fargeSample[0] == svart){
-				Motor.A.setSpeed(100);
+				Motor.A.setSpeed(150);
 				Motor.B.setSpeed(100);
 		 		Motor.A.forward();
 				Motor.B.forward();
@@ -52,10 +53,12 @@ public class relly {
 		 		lejos.hardware.Sound.playSample(au);
 			}else if(lysSample[0] < 0.2){
 				Motor.A.setSpeed(100);
-				Motor.B.setSpeed(100);
+				Motor.B.setSpeed(150);
+				System.out.println(lysSample[0]);
+				Thread.sleep(500);
 			}
 			else  {
-				Motor.A.setSpeed(150);
+				Motor.A.setSpeed(100);
 				Motor.B.setSpeed(100);
 				Motor.A.forward();
 				Motor.B.forward();
