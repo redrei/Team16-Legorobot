498c6631bb3bb60446a6f03d333f41516ca6541d
redrei <spillesen@gmail.com>
2018-09-25 12:15:03 +0200

    n

---
+++ rally/relly.java
@@ -0,0 +1,49 @@
+import lejos.hardware.lcd.*;
+import lejos.hardware.motor.*;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.port.Port;
+import lejos.hardware.Brick;
+import lejos.hardware.BrickFinder;
+import lejos.robotics.SampleProvider;
+
+public class relly {
+	public static void main (String[] args)
+	throws Exception
+	{
+		Motor.A.setSpeed(150);
+		Motor.B.setSpeed(100); //setter motorfart A til 200 og B til 300.
+
+		Motor.A.forward();
+		Motor.B.forward(); //kjører framover motor A og B
+
+	 	Brick brick = BrickFinder.getDefault();
+	 	Port s1 = brick.getPort("S1"); //finner hvilke sensorer som er koblet til
+
+		EV3ColorSensor fargesensor = new EV3ColorSensor(s1);
+		SampleProvider fargeLeser = fargesensor.getMode("RGB");
+		float[] fargeSample = new float[fargeLeser.sampleSize()]; //fargesensor med array og sampleprovider
+
+		int svart = 0;
+		for (int i = 0; i<100; i++){
+			fargeLeser.fetchSample(fargeSample, 0);
+			svart += fargeSample[0]* 100;
+		}
+		svart = svart / 100 + 5; //sammenligner farger, sjekker etter svart
+
+	 	boolean fortsett = true;
+
+		while(fortsett) {
+
+	   		fargeLeser.fetchSample(fargeSample, 0);
+
+       		if (fargeSample[0]*100 == svart){
+		 		Motor.A.setSpeed(150);
+				Motor.B.setSpeed(100);
+			}
+			else  {
+				Motor.A.setSpeed(100);
+				Motor.B.setSpeed(150);
+			}
+		}
+	}
+}
