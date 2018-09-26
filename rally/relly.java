1b94b68f70e28ed5a6c6a24a17340744ee8ed34d
redrei <spillesen@gmail.com>
2018-09-26 14:36:59 +0200

    ny oppdatering

--- rally/relly.java
+++ rally/relly.java
@@ -28,7 +28,7 @@ public class relly {
 		SampleProvider fargeLeser = fargesensor.getColorIDMode();
 
 		NXTLightSensor lyssensor = new NXTLightSensor(s2);
-		SampleProvider lysLeser = lyssensor.getAmbientMode();
+		SampleProvider lysLeser = lyssensor.getRedMode();
 		float[] fargeSample = new float[fargeLeser.sampleSize()];
 		float[] lysSample = new float[lysLeser.sampleSize()];
 //fargesensor med array og sampleprovider
@@ -39,31 +39,42 @@ public class relly {
 
 	   		fargeLeser.fetchSample(fargeSample, 0);
 			lysLeser.fetchSample(lysSample, 0);
-			/*if (fargeSample[0] == svart && lysSample[0] < 0.24){
-				Motor.A.setSpeed(300);
-				Motor.B.setSpeed(300);
+			if (lysSample[0] < 0.42 && fargeSample[0]== 7){
+				Motor.A.setSpeed(550);
+				Motor.B.setSpeed(550);
 				Motor.A.forward();
 				Motor.B.forward();
 				Thread.sleep(200);
 				//System.out.println("0");
 			}
-       		else*/ if (fargeSample[0] == 7){
-				Motor.A.setSpeed(150);
-				Motor.B.setSpeed(200);
+       		else if (fargeSample[0] == 7){
+				Motor.A.setSpeed(200);
+				Motor.B.setSpeed(400);
 		 		Motor.A.forward();
 				Motor.B.backward();
+				Thread.sleep(450);
+				Motor.A.stop(true);
+				Motor.B.stop(true);
+				Thread.sleep(100);
 				//System.out.println("1");
 				//File au = new File("./au.wav");
 		 		//lejos.hardware.Sound.playSample(au);
-			}else if(lysSample[0] < 0.24){
-				Motor.A.setSpeed(300);
-				Motor.B.setSpeed(400);
+		 	}else if(lysSample[0] <= 0.42){
+				Motor.A.setSpeed(400);
+				Motor.B.setSpeed(600);
+				Motor.A.forward();
+				Motor.B.forward();
 				//System.out.println(lysSample[0]);
 				//Thread.sleep(200);
-			}
-			else  {
-				Motor.A.setSpeed(350);
-				Motor.B.setSpeed(350);
+			}else if (fargeSample[0] == 23){
+				Motor.A.setSpeed(550);
+				Motor.B.setSpeed(550);
+				Motor.A.forward();
+				Motor.B.forward();
+				Thread.sleep(2500);
+			}else  {
+				Motor.A.setSpeed(550);
+				Motor.B.setSpeed(550);
 				Motor.A.forward();
 				Motor.B.forward();
 				//Thread.sleep(200);
